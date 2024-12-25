de09eed118175648cd47e98d65779d5092fd6e44
Ahmat-2000 <ahmatmhtlouky@gmail.com>
2024-12-25 17:51:58 +0100

    disabling typescript

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,13 @@
+import Transactions from "@/components/Transactions";
 import Services from "./services/page";
+// import Hr from "@/components/Hr";
 
 export default function Home() {
   return (
     <>
       <Services />
+      {/* <Hr /> */}
+      <Transactions />
     </>
   );
 }
--- frontend/components/Transactions.tsx
+++ frontend/components/Transactions.tsx
@@ -1,7 +1,24 @@
-
+/* eslint-disable @typescript-eslint/no-unused-vars */
+'use client'
+import React, {useContext} from "react";
+import { TransactionsContext } from "@/context/TransactionContext";
+import dummyData from '@/utils/dummyData';
 const Transactions = () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const { currentAccount } : any = useContext(TransactionsContext);
   return (
-    <div>Transactions</div>
+    <div className="">
+      <div className="">
+        { currentAccount ? (
+          <h3 className="text-3xl text-center my-2">Latest Transactions</h3>
+          ) 
+          : (
+          <h3 className="text-3xl text-center my-2">Connect your account to see the latest transactions</h3>
+          )
+
+        }
+      </div>
+    </div>
   )
 };
 
